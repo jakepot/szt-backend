bb4e78700f8310840e475e5716f227e54250b40e
Jakub Pyra <jakub.pyra.1@gmail.com>
2020-01-13 22:04:47 +0100

    Strip find responses

--- api/review/controllers/Review.js
+++ api/review/controllers/Review.js
@@ -1,8 +1,30 @@
-'use strict';
+"use strict";
+const { sanitizeEntity } = require("strapi-utils");
 
 /**
  * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.review.search(ctx.query);
+    } else {
+      entities = await strapi.services.review.find(ctx.query);
+    }
+
+    return entities.map(entity => {
+      const review = sanitizeEntity(entity, { model: strapi.models.review });
+
+      const len = review.text.length;
+      review.text = review.text.slice(0, 500);
+      if (len > 500) {
+        review.text += "...";
+      }
+
+      return review;
+    });
+  }
+};
--- api/song/controllers/Song.js
+++ api/song/controllers/Song.js
@@ -1,8 +1,28 @@
-'use strict';
+"use strict";
+const { sanitizeEntity } = require("strapi-utils");
 
 /**
  * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.song.search(ctx.query);
+    } else {
+      entities = await strapi.services.song.find(ctx.query);
+    }
+
+    return entities.map(entity => {
+      const song = sanitizeEntity(entity, { model: strapi.models.song });
+
+      if (song.review && song.review.text) {
+        delete song.review.text;
+      }
+
+      return song;
+    });
+  }
+};
