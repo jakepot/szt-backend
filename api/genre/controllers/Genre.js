5b82311b8aea98bde064868be514227657add507
Jakub Pyra <jakub.pyra.1@gmail.com>
2020-01-20 20:47:01 +0100

    Remove genre songs details

--- api/genre/controllers/Genre.js
+++ api/genre/controllers/Genre.js
@@ -1,8 +1,26 @@
-'use strict';
+"use strict";
+const { sanitizeEntity } = require("strapi-utils");
 
 /**
  * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.genre.search(ctx.query);
+    } else {
+      entities = await strapi.services.genre.find(ctx.query);
+    }
+
+    return entities.map(entity => {
+      const genre = sanitizeEntity(entity, { model: strapi.models.genre });
+
+      genre.songs = genre.songs.length;
+
+      return genre;
+    });
+  }
+};
